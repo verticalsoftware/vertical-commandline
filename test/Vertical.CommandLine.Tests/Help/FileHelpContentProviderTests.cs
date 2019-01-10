c7b733d88ef644527b0b16376c17ddaa94fac923
Dan Ingalla <daningalla@gmail.com>
2019-01-09 20:41:14 -0700

    Resolve assembly path for help assets - fixes #7

--- test/Vertical.CommandLine.Tests/Help/FileHelpContentProviderTests.cs
+++ test/Vertical.CommandLine.Tests/Help/FileHelpContentProviderTests.cs
@@ -4,19 +4,32 @@
 // MIT license. Please refer to LICENSE.txt in the root directory
 // or refer to https://opensource.org/licenses/MIT
 
+using System.IO;
 using Shouldly;
 using Xunit;
 using Vertical.CommandLine.Help;
 using System.Linq;
+using System.Reflection;
 
 namespace Vertical.CommandLine.Tests.Help
 {
     public class FileHelpContentProviderTests
     {
         [Fact]
-        public void GetInstanceReturnsContent()
+        public void GetInstanceReturnsContentForRelativePath()
         {
             var provider = new FileHelpContentProvider("help.txt");
+            var content = provider.GetInstance();
+
+            content.First().ShouldBe("NAME");
+        }
+        
+        [Fact]
+        public void GetInstanceReturnsContentForRootedPath()
+        {
+            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            var path = Path.GetDirectoryName(assemblyLocation);
+            var provider = new FileHelpContentProvider(Path.Combine(path, "help.txt"));
             var content = provider.GetInstance();
 
             content.First().ShouldBe("NAME");
